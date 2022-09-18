f9c1fe87facec61477e5b17acf9fee3e0db81f88
Your Name <you@example.com>
2022-09-18 17:51:55 +0900

    fix docker-compose.prod

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -10,7 +10,8 @@ elif os.environ.get("DJANGO_SETTINGS_MODULE") == 'config.settings.prod':
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,12 @@
 // vue.config.js
 module.exports = {
-  lintOnSave: false,
-  configureWebpack: {
-    watch: true,
-    plugins: [],
-  },
+	lintOnSave: false,
+	transpileDependencies: ["vuetify"],
+	configureWebpack: config => {
+		if (process.env.NODE_ENV === 'production') {
+			// mutate config for production...
+		} else {
+			// mutate for development...
+		}
+	}
 }
